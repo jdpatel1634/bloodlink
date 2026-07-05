3061a33a802ed49f4378bfe15dbd34a0268364f0
Priyanshu Parmar <paparmar2004@gmail.com>
2026-07-04 20:14:29 -0600

    Add State model

---
+++ app/Models/State.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class State extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Get the cities for the state.
+     */
+    public function cities(): HasMany
+    {
+        return $this->hasMany(City::class);
+    }
+
+    /**
+     * Get the camps for the state.
+     */
+    public function camps(): HasMany
+    {
+        return $this->hasMany(Camp::class);
+    }
+}
