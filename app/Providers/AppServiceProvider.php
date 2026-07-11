9385a9c86ab73062fe319d9307344967ccd4e71c
Arya Gosai <aryagosai2004@gmail.com>
2026-07-11 17:18:49 -0600

    Remove unfinished serology observer registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
-use App\Models\SerologyTest;
-use App\Observers\SerologyTestObserver;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        SerologyTest::observe(SerologyTestObserver::class);
+        //
     }
 }
