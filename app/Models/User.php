887bf7c0e5ba6138e69185f23b802634d2ee2f44
Jaydeep Patel <jaydeeppatel1634@gmail.com>
2026-07-11 19:57:45 -0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
-        'is_super_admin' => 'boolean',
     ];
 
     /**
