cbb5f410c07cb374e6fba5fa68c79b80e91ac084
Priyanshu Parmar <paparmar2004@gmail.com>
2026-07-13 18:07:20 -0600

    Added model for city

---
+++ app/Models/City.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class City extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'state_id',
+        'name',
+    ];
+
+    /**
+     * Get the state that owns the city.
+     */
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    /**
+     * Get the camps for the city.
+     */
+    public function camps(): HasMany
+    {
+        return $this->hasMany(Camp::class);
+    }
+}
