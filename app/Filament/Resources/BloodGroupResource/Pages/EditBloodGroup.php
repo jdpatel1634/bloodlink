abe0270b4b81d85451e8dfcab93ec34c43e7a6ff
Vaidehi Patel <vaidehipatel8490@gmail.com>
2026-07-18 13:43:28 -0600

    Add blood group admin edit page

---
+++ app/Filament/Resources/BloodGroupResource/Pages/EditBloodGroup.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Resources\BloodGroupResource\Pages;
+
+use App\Filament\Resources\BloodGroupResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBloodGroup extends EditRecord
+{
+    protected static string $resource = BloodGroupResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
