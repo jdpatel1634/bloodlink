68db5a05d251dbc41ab9e006e7600e190dcee49f
Arya Gosai <aryagosai2004@gmail.com>
2026-07-23 13:16:57 -0600

    Include donor approval email view

---
+++ resources/views/emails/donors/account-approved.blade.php
@@ -0,0 +1,14 @@
+# Donor Account Approved
+
+Hello {{ $donor->first_name }},
+
+Your donor account has been approved and is now active!
+
+YouYou can now log in to your donor panel:
+
+[Login to Donor Panel]({{ url('/donor') }})
+
+Thank you for being a part of our community.
+
+Regards,
+Blood Bank Management System
