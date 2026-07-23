bf1d1444f8dc0d1505e02293269ad30610b068d9
Priyanshu Parmar <paparmar2004@gmail.com>
2026-07-23 12:53:24 -0600

    Track homepage advertisement records

---
+++ app/Models/Advertisement.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Advertisement extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'image_path',
+        'content',
+        'start_date',
+        'end_date',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+        'is_active' => 'boolean',
+    ];
+}
