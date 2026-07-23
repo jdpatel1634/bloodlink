db617f8be6b089c1218bb7a42710bce012c0dfb3
Priyanshu Parmar <paparmar2004@gmail.com>
2026-07-23 12:18:01 -0600

    Set up blood issue creation page

---
+++ app/Filament/Resources/BloodIssueResource/Pages/CreateBloodIssue.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BloodIssueResource\Pages;
+
+use App\Filament\Resources\BloodIssueResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateBloodIssue extends CreateRecord
+{
+    protected static string $resource = BloodIssueResource::class;
+}
