a90ef071017614b7391cdfb70895cb80e910d49d
Jaydeep Patel <jaydeeppatel1634@gmail.com>
2026-07-23 12:18:41 -0600

    Set up reserved unit creation page

---
+++ app/Filament/Resources/ReservedUnitResource/Pages/CreateReservedUnit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ReservedUnitResource\Pages;
+
+use App\Filament\Resources\ReservedUnitResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateReservedUnit extends CreateRecord
+{
+    protected static string $resource = ReservedUnitResource::class;
+}
